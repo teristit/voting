51421ae0303d8ca582ca63e69d753b3287585379
Ντμίτρι <dimitri.alexandr5w@yandex.ru>
2025-11-01 16:29:46 +0300

    Update models __init__.py with correct imports

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,8 +1,20 @@
 from .user import User
-from .session import Session, SessionParticipant
+from .session import Session, SessionParticipant, BonusParameters
 from .vote import Vote
-from .bonus import BonusParameters
 from .result import Result
 from .auth import AuthSession, RevokedToken
 from .audit import AuditLog
-from .settings import SystemSetting
+from .settings import SystemSettings
+
+__all__ = [
+    'User',
+    'Session',
+    'SessionParticipant', 
+    'BonusParameters',
+    'Vote',
+    'Result',
+    'AuthSession',
+    'RevokedToken',
+    'AuditLog',
+    'SystemSettings'
+]
